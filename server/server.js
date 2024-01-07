a63e438e4f537342888895023af9cdaa4de7511d
thapa technical <vbthapa55@gmail.com>
2024-01-07 20:21:08 +0530

    adding proxy

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const corsOptions = {
     // Check if the origin is allowed
     const allowedOrigins = [
       "http://localhost:5173",
-      // "http://localhost:4173",
-      // "http://thapatechnical.site",
-      // "http://www.thapatechnical.site",
+      "http://localhost:4173",
+      "http://thapatechnical.site",
+      "http://www.thapatechnical.site",
     ];
     const isAllowed = allowedOrigins.includes(origin);
     callback(null, isAllowed ? origin : false);
