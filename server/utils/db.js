4cc6dbe3560e1bb0578741eb53701c1490c49ffb
thapa technical <vbthapa55@gmail.com>
2025-04-29 13:46:57 +0530

    checking error

--- server/utils/db.js
+++ server/utils/db.js
@@ -9,7 +9,7 @@ const connectDb = async () => {
     await mongoose.connect(URI);
     console.log("connection successful to DB");
   } catch (error) {
-    console.error("database connection failed");
+    console.error(error, "database connection failed");
     process.exit(0);
   }
 };
